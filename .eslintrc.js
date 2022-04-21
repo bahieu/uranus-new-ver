964dd4062a02d816eb72e9886e5924b73a037a3f
unknown <thaibahieu7@gmail.com>
2022-04-21 17:31:52 +0700

    upload source code

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'prettier',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: '../my-cra-ts-app/tsconfig.json',
+  },
+};
